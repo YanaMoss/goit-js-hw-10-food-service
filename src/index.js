72ebe77cfd6263c075e694520b420a3fb0e93fb7
Yana Sokurenko <yanamoss@gmail.com>
2021-06-27 01:02:13 +0300

    HW-10 Done

--- src/index.js
+++ src/index.js
@@ -1,38 +1,37 @@
 import "./scss/main.scss";
 import template from "./templates/menu.hbs";
 import data from "./menu.json";
-import { getElementById } from "domutils";
-import { classBody } from "babel-types";
-// import { setLocalStorage, getLocalStorage } from "./utils/utils";
 window.onload = () => {
    const menu = document.querySelector(".js-menu");
-      menu.innerHTML = template(data);
+   menu.innerHTML = template(data);
    
    const page = document.querySelector("body");
-   console.log(page);
    const toggle = document.getElementById("theme-switch-toggle");
-   toggle.addEventListener("change", switchTheme);
-   if (localStorage.getItem('theme') === 'light') {
-      page.classList.add("light-theme");
-      page.classList.remove("dark-theme");
-   }
-      else if (localStorage.getItem('theme') === 'dark') {
-      page.classList.add("dark-theme");
-      page.classList.remove("light-theme");
-      }
 
+   toggle.addEventListener("change", switchTheme);
+   
    function switchTheme(event) {
-      if (page.classList.contains("light-theme") === false && page.classList.contains("dark-theme") === false) {
+      if (!page.classList.contains("light-theme") && !page.classList.contains("dark-theme")) {
          page.classList.add("dark-theme");
          localStorage.setItem('theme', 'dark');
       }
-      else if (page.classList.contains("light-theme") === true) {
+      else if (page.classList.contains("light-theme")) {
          page.classList.replace("light-theme", "dark-theme");
          localStorage.setItem('theme', 'dark');
       }
-      else{
+      else {
          page.classList.replace("dark-theme", "light-theme");
          localStorage.setItem('theme', 'light');
-      }
-   } 
- };
+      };
+   };
+
+    if (localStorage.getItem('theme') === 'light') {
+      page.classList.add("light-theme");
+      page.classList.remove("dark-theme");
+   }
+   else if (localStorage.getItem('theme') === 'dark') {
+      toggle.setAttribute('checked', 'true')
+      page.classList.add("dark-theme");
+      page.classList.remove("light-theme");
+   };
+};
